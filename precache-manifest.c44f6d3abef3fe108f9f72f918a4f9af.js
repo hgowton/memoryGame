b6117047d6cff1a8bea135c95c4b7ea7c49ac03d
hgowton <hgowton@gmail.com>
2020-05-12 21:58:59 -0400

    Updates

---
+++ precache-manifest.c44f6d3abef3fe108f9f72f918a4f9af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5128158ba0055e510d94b8eb44f38233",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f1334d99d2a7c8dd7a4c",
+    "url": "./static/css/main.39326d60.chunk.css"
+  },
+  {
+    "revision": "42619e5dc4b99a9207a5",
+    "url": "./static/js/2.ee3b3c5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ee3b3c5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1334d99d2a7c8dd7a4c",
+    "url": "./static/js/main.119e397e.chunk.js"
+  },
+  {
+    "revision": "141482639f7f4be3f90e",
+    "url": "./static/js/runtime-main.7994ca89.js"
+  }
+]);
